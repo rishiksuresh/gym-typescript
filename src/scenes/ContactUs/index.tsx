c246b6d63afe5aa168aac932d34ef919e7b5d08b
rishiksuresh <ktmrishiks@gmail.com>
2023-07-02 23:11:24 -0700

    first commit

--- src/scenes/ContactUs/index.tsx
+++ src/scenes/ContactUs/index.tsx
@@ -3,6 +3,7 @@ import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
 import { useForm } from "react-hook-form";
 import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
--- src/shared/ActionButton.tsx
+++ src/shared/ActionButton.tsx
@@ -1,4 +1,3 @@
-import { ReactNode } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { SelectedPage } from "./types";
 
@@ -11,7 +10,7 @@ const ActionButton = ({ children, setSelectedPage }: Props) => {
   return (
     <AnchorLink
       className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white"
-      onClick={() => SelectedPage.ContactUs}
+      onClick={() => setSelectedPage(SelectedPage.ContactUs)}
       href={`#${SelectedPage.ContactUs}`}
     >
       {children}
